eb32c1917849c8eaa9586caf80d07f63139e46f4
Charles Sin <charlessin1993@gmail.com>
2022-12-07 21:58:28 +0800

    1. modify cats api return type from string to json

--- src/cats.controller.ts
+++ src/cats.controller.ts
@@ -5,26 +5,26 @@ import { CreateCatDto, UpdateCatDto, ListAllEntities } from './dto';
 export class CatsController {
   @Post()
   create(@Body() createCatDto: CreateCatDto) {
-    return 'This action adds a new cat';
+    return { msg: 'This action adds a new cat' };
   }
 
   @Get()
   findAll(@Query() query: ListAllEntities) {
-    return `This action returns all cats (limit: ${query} items)`;
+    return { msg: `This action returns all cats (limit: ${query} items)` };
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return `This action returns a #${id} cat`;
+    return { msg: `This action returns a #${id} cat` };
   }
 
   @Put(':id')
   update(@Param('id') id: string, @Body() updateCatDto: UpdateCatDto) {
-    return `This action updates a #${id} cat`;
+    return { msg: `This action updates a #${id} cat` };
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return `This action removes a #${id} cat`;
+    return { msg: `This action removes a #${id} cat` };
   }
 }
