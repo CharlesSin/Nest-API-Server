5b3e48539781671174b9ed33eba3e876348bdd6a
Charles Sin <charlessin1993@gmail.com>
2022-12-24 15:58:28 +0800

    1. add static server file.

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import cors from 'cors';
 import { AppModule } from './app.module';
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.useStaticAssets(join(__dirname, '..', 'public'));
-  
+
   await app.listen(4116);
 }
 bootstrap();
